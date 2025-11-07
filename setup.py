a09bc12a012cbfc66711c34aaa8b2a3a0d7c730b
stevenblum@uri.edu <StevenBlum@uri.edu>
2025-11-07 18:48:20 -0500

    Home, Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 import os
 from glob import glob
-from setuptools import setup
+from setuptools import setup, find_packages   # <-- change here
 
 package_name = 'ros2_stacks'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(include=['ros2_stacks', 'ros2_stacks.*']),  # <-- change here
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        # The correct format using os.path.join and glob
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        ('share/' + package_name, ['package.xml']), # It's good practice to also install the package.xml
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,7 +21,7 @@ setup(
     license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'ros2_stacks = ros2_stacks.ros2_stacks:main',
+            'ros2_stacks = ros2_stacks.ros2_stacks:main',  # <-- correct for flattened structure
         ],
     },
 )
